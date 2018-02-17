20e6d93beb38f160212695910a8007de80b62bb4
Edward Gomez <edward@gomez.me.uk>
2018-02-17 17:12:29 +0000

    Remote import

--- weather/monitor.py
+++ weather/monitor.py
@@ -2,7 +2,20 @@ import os
 from datetime import datetime
 from sense_hat import SenseHat
 
-from main import get_smooth, get_cpu_temp
+def get_cpu_temp():
+  res = os.popen("vcgencmd measure_temp").readline()
+  t = float(res.replace("temp=","").replace("'C\n",""))
+  return t
+
+# use moving average to smooth readings
+def get_smooth(x):
+  if not hasattr(get_smooth, "t"):
+    get_smooth.t = [x,x,x]
+  get_smooth.t[2] = get_smooth.t[1]
+  get_smooth.t[1] = get_smooth.t[0]
+  get_smooth.t[0] = x
+  xs = (get_smooth.t[0]+get_smooth.t[1]+get_smooth.t[2])/3
+  return xs
 
 def monitor():
     sense = SenseHat()
@@ -15,6 +28,7 @@ def monitor():
     f = open("templog.txt", "a+")
     f.write("{},{}".format(datetime.utcnow('T'), t_corr))
     f.close()
+    return
 
 
 if __name__ == '__main__':
